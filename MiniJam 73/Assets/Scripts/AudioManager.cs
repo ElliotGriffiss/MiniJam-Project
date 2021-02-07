95cc9abef8515e963e03066bcb49aef986f97685
Jordan Fite <jordantfite@gmail.com>
2021-02-07 17:31:03 -0600

    Finished sound effect events

--- MiniJam 73/Assets/Scripts/AudioManager.cs
+++ MiniJam 73/Assets/Scripts/AudioManager.cs
@@ -11,6 +11,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource levelMusicDrums;
     [SerializeField] private AudioSource levelMusicBass;
     [SerializeField] private AudioSource levelMusicSynth;
+    [SerializeField] private AudioSource levelComplete;
 
     [Header("Sound Effect References")]
     [SerializeField] private AudioSource soundEffectDeath;
@@ -18,50 +19,55 @@ public class AudioManager : MonoBehaviour
 
     [Header("Music Sync Settings")]
     [SerializeField] private float bpm = 150f;
-    private int beats = 32;
-    private float beatsPerSecond = 2.5f;
+    private int beats = 128;
+    private float beatInterval = .625f;
 
     private int musicIndex;
+    private bool shouldPlaySwitchSound;
 
-    // Start is called before the first frame update
     void Start()
     {
         CharacterController.OnPlayerMove += CycleAudio;
         CharacterController.OnNeutralTriggerActivated += PlayAllAudio;
-        CharacterController.OnPlayerDeath += PlayAllAudio;
+        CharacterController.OnPlayerDeath += PlayerDeathAudio;
+        CharacterController.OnPlayerCompleteLevel += PlayLevelCompleteMusic;
         musicIndex = -1;
+        shouldPlaySwitchSound = true;
     }
 
     private void CycleAudio()
     {
+        // Increment index of instrument to play
         musicIndex++;
-
         if (musicIndex > 2)
         {
             musicIndex = 0;
         }
 
-        Debug.Log(levelMusicDrums.time);
-
-        /*
-
-        Music notes
-        150 / 60 = 2.5 beats per second
-        2.5*  12.8 = 32 beats in the whole track
-
-        At a given time, 6 seconds into the track:
-        6 / 12.8 = .46875
-        .46875 * 32 = currently at beat 15 of 32
-
-        */
-
+        // Calculate delay for switching instrument on beat
         double currentTime = levelMusicDrums.time / 12.8;
-        double currentBeat = currentTime * 32;
-        double delay = currentBeat - Mathf.Floor((float)currentBeat);
+        double currentBeat = currentTime * beats;
+        float delay = (float)(Mathf.CeilToInt((float)currentBeat) - currentBeat) * beatInterval;
+        StartCoroutine(WaitUntilNextBeat(delay, musicIndex));
+
+        // Determine if light switch should play
+        if (shouldPlaySwitchSound) {
+            shouldPlaySwitchSound = false;
+            soundEffectLightSwitch.Play();
+        }        
     }
 
     private IEnumerator WaitUntilNextBeat(float delay, int musicIndex)
     {
+        /*
+            Music notes
+            150 / 60 = 2.5 beats per second
+            1.25 * 12.8 = 64 beats in the whole track
+
+            At a given time, 6 seconds into the track:
+            6 / 12.8 = .46875
+            .46875 * 64 = currently at beat 15 of 32
+        */
         yield return new WaitForSeconds(delay);
 
         if (musicIndex == 0)
@@ -86,11 +92,25 @@ public class AudioManager : MonoBehaviour
 
     private void PlayAllAudio()
     {
+        // Reset flags
         musicIndex = -1;
+        shouldPlaySwitchSound = true;
+
+        // Unmute music
         levelMusicDrums.mute = false;
         levelMusicBass.mute = false;
         levelMusicSynth.mute = false;
-        StartCoroutine(WaitUntilNextBeat(0, musicIndex));
+    }
+
+    private void PlayLevelCompleteMusic()
+    {
+        levelComplete.Play();
+    }
+
+    private void PlayerDeathAudio()
+    {
+        PlayAllAudio();
+        soundEffectDeath.Play();
     }
 
     public void ChangeMusicAudioLevel(float newLevel)
@@ -105,8 +125,10 @@ public class AudioManager : MonoBehaviour
 
     void OnDestroy()
     {
+        // Clean up event connections
         CharacterController.OnPlayerMove -= CycleAudio; 
         CharacterController.OnNeutralTriggerActivated -= PlayAllAudio;
-        CharacterController.OnPlayerDeath -= PlayAllAudio;
+        CharacterController.OnPlayerDeath -= PlayerDeathAudio;
+        CharacterController.OnPlayerCompleteLevel -= PlayLevelCompleteMusic;
     }
 }
