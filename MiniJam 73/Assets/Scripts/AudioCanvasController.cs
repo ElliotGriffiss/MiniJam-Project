0704b5325fab95942aecdaf285a4699fb5198efb
Elliot <elliotgriffiss@gmail.com>
2021-02-08 04:34:34 +0000

    Implements Music and SFX Settings Menu

---
+++ MiniJam 73/Assets/Scripts/AudioCanvasController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioCanvasController : MonoBehaviour
+{
+    [SerializeField] private GameObject CanvasParent;
+    [SerializeField] private AudioMixer AudioMixer;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            CanvasParent.SetActive(!CanvasParent.activeInHierarchy);
+        }
+    }
+
+    public void OnMusicSliderUpdated(float Value)
+    {
+        Debug.Log(Value);
+        AudioMixer.SetFloat("musicLevel", Value);
+    }
+
+    public void OnSFXSliderUpdated(float Value)
+    {
+        AudioMixer.SetFloat("soundEffectLevel", Value);
+    }
+}
